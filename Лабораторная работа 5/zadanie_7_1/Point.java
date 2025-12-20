8a229e487c8f169c230085780f6c0eec285a48f6
b1eva <145598767+Daniil393@users.noreply.github.com>
2025-12-20 21:27:15 +0500

    Add files via upload

---
+++ Лабораторная работа 5/zadanie_7_1/Point.java
@@ -0,0 +1,32 @@
+package zadanie_7_1;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+
+    @Override
+    public String toString() {
+        return "{" + x + ";" + y + "}";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Point)) return false;
+        Point p = (Point) obj;
+        return Double.compare(x, p.x) == 0 && Double.compare(y, p.y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) * 31 + Double.hashCode(y);
+    }
+}
