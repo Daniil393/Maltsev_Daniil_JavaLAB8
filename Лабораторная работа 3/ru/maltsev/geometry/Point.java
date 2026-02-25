a79a959fc8b1b2a7fdd57099498571df360d3e99
b1eva <145598767+Daniil393@users.noreply.github.com>
2026-02-25 14:13:22 +0500

    Update Point.java

--- Лабораторная работа 3/ru/maltsev/geometry/Point.java
+++ Лабораторная работа 3/ru/maltsev/geometry/Point.java
@@ -1,27 +1,64 @@
 package ru.maltsev.geometry;
 
+/**
+ * Класс {@code Point} представляет точку на двумерной плоскости
+ * с координатами X и Y.
+ */
 public class Point {
+    /** Координата по оси X. */
     private double x;
+
+    /** Координата по оси Y. */
     private double y;
 
+    /**
+     * Создаёт точку с заданными координатами.
+     *
+     * @param x координата по оси X
+     * @param y координата по оси Y
+     */
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Возвращает координату по оси X.
+     *
+     * @return значение X
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Возвращает координату по оси Y.
+     *
+     * @return значение Y
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Возвращает строковое представление точки
+     * в формате {x;y}.
+     *
+     * @return строка с координатами точки
+     */
     @Override
     public String toString() {
         return "{" + x + ";" + y + "}";
     }
 
+    /**
+     * Сравнивает текущую точку с другим объектом.
+     * Точки считаются равными, если их координаты X и Y совпадают.
+     *
+     * @param obj объект для сравнения
+     * @return true, если объект является точкой с теми же координатами,
+     *         иначе false
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
