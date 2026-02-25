f0847cee145476aada2994957df6fa57e29e176c
b1eva <145598767+Daniil393@users.noreply.github.com>
2026-02-25 14:16:44 +0500

    Update AutomaticGun3.java

--- Лабораторная работа 3/ru/maltsev/weapons/AutomaticGun3.java
+++ Лабораторная работа 3/ru/maltsev/weapons/AutomaticGun3.java
@@ -1,9 +1,28 @@
 package ru.maltsev.weapons;
 
+/**
+ * Класс {@code AutomaticGun3} представляет автоматическое оружие,
+ * наследуемое от {@code Weapon2}.
+ * <p>
+ * Автомат стреляет очередями с заданной скорострельностью.
+ * При каждом выстреле количество патронов уменьшается.
+ * Если патроны заканчиваются, выводится сообщение "Клац!",
+ * и стрельба прекращается.
+ */
 public class AutomaticGun3 extends Weapon2 {
 
+    /** Скорострельность (выстрелов в секунду). */
     private final int rateOfFire;
 
+    /**
+     * Создаёт автомат с указанным количеством патронов
+     * и скорострельностью.
+     *
+     * @param ammo       начальное количество патронов
+     * @param rateOfFire скорострельность (выстрелов в секунду),
+     *                   должна быть положительной
+     * @throws IllegalArgumentException если {@code rateOfFire <= 0}
+     */
     public AutomaticGun3(int ammo, int rateOfFire) {
         super(ammo);
 
@@ -13,6 +32,13 @@ public class AutomaticGun3 extends Weapon2 {
         this.rateOfFire = rateOfFire;
     }
 
+    /**
+     * Производит очередь выстрелов.
+     * Количество попыток выстрела равно скорострельности.
+     * <p>
+     * При наличии патронов выводится "Бах!" и патрон уменьшается.
+     * При отсутствии патронов выводится "Клац!", после чего стрельба прекращается.
+     */
     @Override
     public void shoot() {
         for (int i = 0; i < rateOfFire; i++) {
@@ -26,6 +52,13 @@ public class AutomaticGun3 extends Weapon2 {
         }
     }
 
+    /**
+     * Производит стрельбу в течение указанного количества секунд.
+     *
+     * @param seconds продолжительность стрельбы (в секундах),
+     *                должна быть положительной
+     * @throws IllegalArgumentException если {@code seconds <= 0}
+     */
     public void shoot(int seconds) {
         if (seconds <= 0)
             throw new IllegalArgumentException("Время должно быть положительным");
@@ -41,10 +74,21 @@ public class AutomaticGun3 extends Weapon2 {
         }
     }
 
+    /**
+     * Возвращает скорострельность автомата.
+     *
+     * @return количество выстрелов в секунду
+     */
     public int getRateOfFire() {
         return rateOfFire;
     }
 
+    /**
+     * Возвращает строковое представление автомата.
+     *
+     * @return строка в формате:
+     * "Автомат [патроны: X, скорострельность: Y]"
+     */
     @Override
     public String toString() {
         return "Автомат [патроны: " + ammo + ", скорострельность: " + rateOfFire + "]";
