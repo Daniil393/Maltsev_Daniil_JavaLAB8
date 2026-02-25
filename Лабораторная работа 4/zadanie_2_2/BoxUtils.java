a7e035ee1c41873647696cbf5274875fee1e2d7c
b1eva <145598767+Daniil393@users.noreply.github.com>
2026-02-25 14:34:43 +0500

    Update BoxUtils.java

--- Лабораторная работа 4/zadanie_2_2/BoxUtils.java
+++ Лабораторная работа 4/zadanie_2_2/BoxUtils.java
@@ -3,8 +3,22 @@ package zadanie_2_2;
 import zadanie_1_1.Box;
 import java.util.Collection;
 
+/**
+ * Утилитный класс для работы с коллекциями {@link Box} объектов.
+ * <p>
+ * Предоставляет методы для поиска максимальных значений в наборах коробок.
+ */
 public class BoxUtils {
 
+    /**
+     * Находит максимальное числовое значение среди всех непустых коробок в коллекции.
+     * <p>
+     * Игнорирует пустые коробки. Если все коробки пустые, выбрасывает {@link IllegalArgumentException}.
+     *
+     * @param boxes коллекция коробок, содержащих объекты, расширяющие {@link Number}
+     * @return максимальное значение среди содержимого всех непустых коробок
+     * @throws IllegalArgumentException если ни одна коробка не содержит значения
+     */
     public static double findMax(Collection<? extends Box<? extends Number>> boxes) {
         double max = Double.NEGATIVE_INFINITY;
         boolean found = false;
