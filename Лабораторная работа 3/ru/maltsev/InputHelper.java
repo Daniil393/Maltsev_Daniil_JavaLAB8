4861afb357f59afcbe1484196801ca185c52358b
b1eva <145598767+Daniil393@users.noreply.github.com>
2026-02-25 14:21:19 +0500

    Update InputHelper.java

--- Лабораторная работа 3/ru/maltsev/InputHelper.java
+++ Лабораторная работа 3/ru/maltsev/InputHelper.java
@@ -2,8 +2,24 @@ package ru.maltsev;
 
 import java.util.Scanner;
 
+/**
+ * Утилитный класс для работы с вводом пользователя.
+ * <p>
+ * Предоставляет методы для безопасного считывания данных
+ * с консоли с проверкой формата.
+ */
 public class InputHelper {
 
+    /**
+     * Считывает строку, содержащую целое число, с консоли.
+     * <p>
+     * Метод повторяет запрос до тех пор, пока пользователь
+     * не введёт корректное целое число.
+     *
+     * @param sc     объект {@link Scanner} для чтения ввода
+     * @param prompt сообщение, которое выводится пользователю
+     * @return строка, содержащая корректное целое число
+     */
     public static String readNumberString(Scanner sc, String prompt) {
         while (true) {
             System.out.print(prompt);
